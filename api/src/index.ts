7d3ce16d6ee2356b7a26b7289a90887256aa3423
Benord <Baynaabj749@gmail.com>
2024-10-07 20:24:27 +0800

    backend cloudinary

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./database/connectionDB";
+import Multer, { memoryStorage } from "multer";
+import { v2 as cloudinary } from "cloudinary";
 import {
   authRouter,
   categoryRouter,
@@ -24,8 +26,35 @@ app.use("/product", productRouter);
 app.use("/category", categoryRouter);
 app.use("/review", reviewRouter);
 
-app.get("/", (_req, res) => {
-  res.json({ message: "Hello, world!" });
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+
+const storage = memoryStorage();
+
+const upload = Multer({ storage });
+
+async function handleUpload(file: string) {
+  const res = await cloudinary.uploader.upload(file, {
+    resource_type: "auto",
+  });
+  return res;
+}
+
+app.post("/upload", upload.single("image"), async (req, res) => {
+  if (!req.file) return res.status(400).send("No file uploaded.");
+
+  try {
+    const b64 = Buffer.from(req.file.buffer).toString("base64");
+    let dataURI = "data:" + req.file.mimetype + ";base64," + b64;
+    const cldRes = await handleUpload(dataURI);
+    res.json(cldRes);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error uploading file to cloudinary.");
+  }
 });
 
 app.listen(PORT, () => {
